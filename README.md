4f61be98cfc5d671bc5eff97b0af172dcc7f1501
CaptainNox <24610075+CaptainNox@users.noreply.github.com>
2024-05-21 15:15:01 +0200

    Added iteration through instructions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Simple x86 emulator
 
 In this project, I am writing a simple 32-bit x86 CPU emulator to learn how emulators work under the hood.
+
+## Resources
+
+ - [Capstone documentation](https://www.capstone-engine.org/lang_python.html)
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 #!/bin/python3
-
 from capstone import *
 from capstone.x86 import *
 
-instructions = ""
-with open("shellcode/add.bin", "rb") as bin:
-    instructions = bin.read()
+# Implementation of x86 CPU registers
+registers = {}
+registers[X86_REG_EAX] = 0
+registers[X86_REG_EBX] = 0
 
-print(instructions)
+# Implementation for the CPU instruction pointer
+registers[X86_REG_EIP] = 0
+
+# Reading bytes of binary
+code = ""
+with open("shellcode/add.bin", "rb") as bin:
+    code = bin.read()
+
+# Setting up capstone
+cs = Cs(CS_ARCH_X86, CS_MODE_32)
+cs.detail = True
+
+# Iterate through all instructions
+while registers[X86_REG_EIP] != len(code):
+    addr = registers[X86_REG_EIP]
+    instruction = next(cs.disasm(code[addr:addr + 15], addr))
+    print(f"Instruction ({addr}): {instruction.mnemonic}")
+
+    # Incrementing EIP
+    registers[X86_REG_EIP] += instruction.size
+
+print("Done, iterated through all instructions")
+
